f6538787b66a0624155574cf94fe318d6f71c741
Valentin <valentin.moretpro@gmail.com>
2021-10-27 14:20:03 +0200

    fix: list container

---
+++ src/components/Settings/SettingsCommandsList.tsx
@@ -0,0 +1,43 @@
+import { FC } from "react";
+import SettingsCommandsTitle from "./SettingsCommandsTitle";
+import SettingsCommandsListItem from "./SettingsCommandsListItem";
+import { SettingsCommandsItem } from "../../domain/SettingsCommandsItem";
+import styled from "styled-components";
+
+const settingsCommandsItems: Array<SettingsCommandsItem> = [
+  {
+    title: "Extrait audio",
+    command: '$ -audio -s X -d Y -i "input"'
+  },
+  {
+    title: "Extrait audio",
+    command: '$ -audio -s X -d Y -i "input"'
+  }
+];
+
+const SettingsCommandsListContainer = styled.div`
+  padding: 20px;
+	margin-top: 10rem;
+  border-radius: 10px;
+`;
+
+const SettingsCommandsListMainContainer = styled.div`
+  margin-right: 5rem;
+`;
+
+const SettingsCommandsList: FC = () => {
+  return (
+		<SettingsCommandsListMainContainer className="column hero is-fullwidth is-flex is-align-items-end">
+    <SettingsCommandsListContainer className="has-background-grey-darker column is-three-quarters">
+      <SettingsCommandsTitle title="Gestion des commandes" />
+      {settingsCommandsItems.map(item => {
+        return (
+          <SettingsCommandsListItem title={item.title} command={item.command} />
+        );
+      })}
+    </SettingsCommandsListContainer>
+		</SettingsCommandsListMainContainer>
+  );
+};
+
+export default SettingsCommandsList;
