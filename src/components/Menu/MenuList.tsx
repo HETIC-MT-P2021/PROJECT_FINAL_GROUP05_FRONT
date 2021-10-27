584e4aa46f51371f932eef8ca880f806031ee84b
Valentin <valentin.moretpro@gmail.com>
2021-10-27 10:21:56 +0200

    feat: add routes and urls in menu

--- src/components/Menu/MenuList.tsx
+++ src/components/Menu/MenuList.tsx
@@ -1,11 +1,12 @@
-import { FC } from "react";
+import {FC} from "react";
 import MenuListItem from "./MenuListItem";
 import MenuListTitle from "./MenuListTitle";
 import { MenuItem } from "../../domain/MenuItem";
 
 interface MenuListProps {
   items: Array<MenuItem>;
-  title: String;
+  title: string;
+	url: string
 }
 
 const MenuList: FC<MenuListProps> = ({ items, title }: MenuListProps) => {
@@ -14,7 +15,7 @@ const MenuList: FC<MenuListProps> = ({ items, title }: MenuListProps) => {
       <MenuListTitle title={title} />
       <ul className="is-flex is-flex-direction-column is-flex-justify-content-center has-text-white">
         {items.map(item => {
-          return <MenuListItem name={item.name} icon={item.icon} />;
+          return <MenuListItem name={item.name} icon={item.icon} url={item.url}/>;
         })}
       </ul>
     </div>
